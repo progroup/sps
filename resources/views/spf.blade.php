6ab42050a76cb12044bc8de567c02e212c39dc3e
Victor Tolbert <victor.tolbert@gmail.com>
2017-11-05 20:12:51 -0500

    wip...

--- resources/views/spf.blade.php
+++ resources/views/spf.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<section class="hero is-medium is-primary is-bold">
+<section class="hero is-medium is-primary is-bold" style="background-size: cover; background-image: url('/images/about.jpg')">
     <div class="hero-body">
         <div class="container">
 
