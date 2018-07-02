059d80127969adf73162ddf1409a18e1164d47d7
Victor Tolbert <victor.tolbert@gmail.com>
2018-07-01 23:52:36 -0400

    Add missing columns wrapper

--- resources/assets/js/views/index.vue
+++ resources/assets/js/views/index.vue
@@ -139,25 +139,27 @@
         <section class="section">
             <div class="container content is-medium">
                 <h2 class="is-size-3">Georgia Prevention Media Showcase</h2>
-                <div class="column is-3">
-                    <div class="content">
-                        <a href="/assets/media/dawson-co-voices-billboard.pdf">
-                            <img src="/assets/media/dawson-co-voices-billboard.png">
-                        </a>
+                <div class="columns">
+                    <div class="column is-3">
+                        <div class="content">
+                            <a href="/assets/media/dawson-co-voices-billboard.pdf">
+                                <img src="/assets/media/dawson-co-voices-billboard.png">
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="column is-3">
-                    <div class="content">
-                        <a href="/assets/media/forsyth-tobacco-cards.pdf">
-                            <img src="/assets/media/forsyth-tobacco-cards.png">
-                        </a>
+                    <div class="column is-3">
+                        <div class="content">
+                            <a href="/assets/media/forsyth-tobacco-cards.pdf">
+                                <img src="/assets/media/forsyth-tobacco-cards.png">
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="column is-3">
-                    <div class="content">
-                        <a href="/assets/media/hall-co-gdbd-football.pdf">
-                            <img src="/assets/media/hall-co-gdbd-football.png">
-                        </a>
+                    <div class="column is-3">
+                        <div class="content">
+                            <a href="/assets/media/hall-co-gdbd-football.pdf">
+                                <img src="/assets/media/hall-co-gdbd-football.png">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
